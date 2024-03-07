5fceed2866c0c5fdbd7a99219b4e178d1d70525c
bobbyhadz <opmftw@gmail.com>
2024-03-07 16:05:50 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('bobbyhadz.com');
+
+const box = document.getElementById('box');
+
+const paragraph = document.createElement('p');
+
+paragraph.innerHTML = `google.<b>com</b>`;
+
+box.appendChild(paragraph);
